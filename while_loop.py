a109e7cd5169d921dfdbda0518d38fc67566cdff
SzMartin1 <martinszabo775@gmail.com>
2022-07-10 20:28:21 +0200

    while loop in python

---
+++ while_loop.py
@@ -0,0 +1,12 @@
+#while loop: a statement that will execute it's block of code,
+#            as long as it's condition remains true.
+
+#while 1==1:
+ #   name = print("Help, im stuck in a loop!")
+
+name = None
+
+while not name:
+    name=input("What's your name?: ")
+
+print("Hello"+ name)
