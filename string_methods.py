8b2e83be88dc1dbe536fac072a6618dd850e36ed
SzMartin1 <martinszabo775@gmail.com>
2022-07-10 17:47:01 +0200

    some string methods in python

---
+++ string_methods.py
@@ -0,0 +1,20 @@
+name = "Martin"
+
+print(len(name)) # megadja a string hosszát
+
+print(name.find("r")) # megadja a karakter helyzetét
+
+print(name.capitalize())
+
+print(name.upper())
+print(name.lower())
+
+print(name.isdigit())
+
+print(name.isalpha())
+
+print(name.count("n"))
+
+print(name.replace("r","t"))
+
+print(name*3)
