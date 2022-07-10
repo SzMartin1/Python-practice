8ad0d0a9f1a1d2254e5e1405a8066b800b10144e
SzMartin1 <martinszabo775@gmail.com>
2022-07-10 18:39:13 +0200

    using if statements in python

---
+++ if_statements.py
@@ -0,0 +1,17 @@
+# if statement is a block of code which will only execute if it's condition is true
+age= int(input("How old are you?: "))
+
+if age == 100:
+    print("Wow, you're a century old bro!")
+elif age >= 18:
+    print("Wow, you are an adult!")
+elif age < 0:
+    print("You haven't been born yet bro!")
+else:
+    print("Wow, you're a child!")
+
+
+# az if statement-ek sorrendje számit!!
+
+ 
+
