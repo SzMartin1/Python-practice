d424e95ba4a9771c324256bfa5a0e43fe1ccf48b
SzMartin1 <martinszabo775@gmail.com>
2022-07-10 18:09:26 +0200

    mathematical functions in python

---
+++ math_func.py
@@ -0,0 +1,18 @@
+import math
+
+pi=3.14
+
+print(round(pi)) # kerekítés
+print(math.ceil(pi)) # szintén kerekítés, csak felfelé
+print(math.floor(pi)) # szintén kerekítés, csak lefelé
+print(abs(pi)) #abszolútérték
+print(pow(pi,2)) # n-edik hatványra emeli az adott számot
+print(math.sqrt(420)) # gyökvonás
+
+x=1
+y=2
+z=3
+
+print(max(x,y,z)) # megkeresi a legnagyobb értékű változót 
+
+print(min(x,y,z)) #megkeresi a legkisebb értékű változót
