ea44c3ac79acc6d3fafb641bb74c676364e4dea2
SzMartin1 <martinszabo775@gmail.com>
2022-07-10 18:04:23 +0200

    using input() in python

---
+++ user_input.py
@@ -0,0 +1,11 @@
+#input() gives a string datatype
+name= input("What is your name?: ")
+print("Hello "+name)
+
+age = int(input("What is your age?: ")) #ha int-et akarunk, akkor castolni kell
+print("Cool, your age is "+ str(age))
+
+height=float(input("How tall are you?: "))
+
+print("So your height is " +str(height))
+
