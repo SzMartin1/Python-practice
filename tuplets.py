7e0ae26c2c7f0cab3a2342795135188b748833cf
SzMartin1 <martinszabo775@gmail.com>
2022-07-11 16:32:34 +0200

    tuplets in python

---
+++ tuplets.py
@@ -0,0 +1,18 @@
+#tuple = -collection which is ordered and unchangeable
+#        -used to group together related data
+
+
+
+
+student= ("Martin",21,"male")
+
+print(student.count("Martin"))
+print(student.index("male"))
+
+for x in student:
+    print(x)
+
+if "Martin" in student:
+    print("Martin is here")
+
+ 
