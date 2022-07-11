c7f45385bf642616aaaf6667cfa5ba450ffb8d42
SzMartin1 <martinszabo775@gmail.com>
2022-07-11 16:27:54 +0200

    multi dimensional lists in python

---
+++ 2d_lists.py
@@ -0,0 +1,9 @@
+#2D lists = a list of lists
+
+drinks=["vodka","whiskey","coffe","soda"]
+dinner=["pizza","hamburger","hotdog"]
+dessert=["cake","ice cream"]
+
+food = [drinks,dinner,dessert]
+
+print(food[0][0]) #2 sets of squared brackets
