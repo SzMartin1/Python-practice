5b4515826a6866fc105f8358bd26a7a8a0c04a16
SzMartin1 <martinszabo775@gmail.com>
2022-07-11 17:16:42 +0200

    functions in python

---
+++ function.py
@@ -0,0 +1,14 @@
+#function = block of code which is executed only when it is called
+
+
+
+
+def hello(first_name,last_name,age):
+    print("Hello "+first_name+" "+last_name+"!")
+    print("You are "+str(age)+" years old!")
+    print("Have a wonderful day!")
+
+my_first_name = "Martin"
+my_last_name = "Szabó"
+my_age = 21
+hello(my_first_name,my_last_name,my_age)
